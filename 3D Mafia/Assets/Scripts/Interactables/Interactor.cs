3c26e29ae9a12b6cc7c793242c357d62d50d708a
YashTelang <ytelang11@gmail.com>
2022-11-03 12:12:44 -0500

    Player objective UI
    Created player objective UI and syncing to when objectives are completed

--- 3D Mafia/Assets/Scripts/Interactables/Interactor.cs
+++ 3D Mafia/Assets/Scripts/Interactables/Interactor.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using TMPro;
+using System.Collections;
 
 public class Interactor : MonoBehaviour
 {
@@ -11,20 +13,33 @@ public class Interactor : MonoBehaviour
     [SerializeField] private int _numFound;
 
     private IInteractable interactable;
+    private TextMeshProUGUI changeText;
+    public TextMeshProUGUI ballText;
+    public TextMeshProUGUI scanText;
+    public TextMeshProUGUI whiteboardText;
+    public TextMeshProUGUI targetText;
 
     private void Update() {
         _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
 
         if (_numFound > 0) {
             interactable = _colliders[0].GetComponent<IInteractable>();
+            
 
             if (interactable != null) {
                 if (!InteractionPromptUI.IsDisplayed) {
                     InteractionPromptUI.SetUp("Interact!");
                 }
 
+                if (interactable is Ball) {
+                    changeText = ballText;
+                } else if (interactable is Scanner) {
+                    changeText = scanText;
+                }
+
                 if (Input.GetKeyDown("e")) {
                     interactable.Interact(this);
+                    changeTextColor();
                 }
             } 
 
@@ -45,5 +60,10 @@ public class Interactor : MonoBehaviour
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(_interactionPoint.position, _interactionPointRadius);
+    }
+
+    public void changeTextColor()
+    {
+        changeText.color = new Color32(0, 255, 0, 255);
     }
 }
--- 3D Mafia/Assets/Scripts/Interactables/Scanner.cs
+++ 3D Mafia/Assets/Scripts/Interactables/Scanner.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scanner : MonoBehaviour
+public class Scanner : MonoBehaviour, IInteractable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private string _prompt;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public string InteractionPrompt => _prompt;
+    private Vector3 scaleChange, positionChange;
+
+    public bool Interact(Interactor interactor) {
+
+        var inventory = interactor.GetComponent<Inventory>();
+
+        if (inventory == null) {
+            return false;
+        }
+
+        if (inventory.HasKey) {
+            Debug.Log("Scaling size of scanner!");
+            scaleChange = new Vector3(0.1f, 0.1f, 0.1f);
+            this.transform.localScale += scaleChange;
+            return true;
+        }
+
+        Debug.Log("No key found!");
+        return false;
     }
 }
